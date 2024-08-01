f8265f582182e5a002e1fa6ed3e8cf7bbe4912f0
chunduri manzitsai <manzit12@gmail.com>
2024-08-02 05:06:24 +0530

    Episode-1

---
+++ App.js
@@ -0,0 +1,16 @@
+// const heading = React.createElement("h1",{id:"heading",xyz:"abc"},"Heading from React");
+// console.log(heading)//object
+// const root=ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);// render mehtod will create h1 tag
+
+
+const parent=React.createElement
+("div",
+    {id:"parent"}
+    ,React.createElement("div",{id:"child"},
+        [React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"i am h2 tag"),]
+
+))
+ console.log(parent)//object
+ const root=ReactDOM.createRoot(document.getElementById("root"));
+ root.render(parent);// render mehtod will create h1 tag
