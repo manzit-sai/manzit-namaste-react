8f7517b715313730e13b602de6d9a3101f806d6a
manzit-sai <manzit12@gmail.com>
2024-08-23 03:54:21 +0530

    episode 3 code

--- App.js
+++ App.js
@@ -6,13 +6,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const parent=React.createElement
-("div",
-    {id:"parent"}
-    ,React.createElement("div",{id:"child"},
-        [React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"i am h2 tag"),]
+//React.createElement => Object => HTML Element(render)
 
-))
- console.log(parent)//object
- const root=ReactDOM.createRoot(document.getElementById("root"));
- root.render(parent);// render mehtod will create h1 tag
+//JSX=>React.createElement=>ReactElement-Js object => HTML Element (render)
+//JSX -HTML like or XML like syntax
+//React Element 
+const Title =()=>(
+<h1 className="head" tabIndex="2">Namaste React by JSX</h1>
+)
+
+const number =10000;
+//Functional Component
+//Component name should be in pascal case
+const HeadingComponent=()=>(
+    <div>
+        <Title/>
+        {number}
+       <h1>Namaste React Functional Component</h1>
+    </div>
+     
+)
+const HeadingComponent2=()=>(
+     <h1>Namaste React Functional Component</h1>
+)
+const root=ReactDOM.createRoot(document.getElementById("root"))
+root.render(<HeadingComponent/>);
+
+
